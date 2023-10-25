983c1b4ec2dcd36beae0f810b4b618b9645d6d5c
Rocky2105 <a20300699@ceti.mx>
2023-10-25 00:13:18 -0600

    Detectar usuario login

--- Program.cs
+++ Program.cs
@@ -8,107 +8,107 @@ bool loggedIn = false;
 
 // Iniciar sesión
 while (!loggedIn)
-{
-Console.WriteLine("=== LogIn / SignUp ===");
-Console.WriteLine("1. Iniciar Sesion");
-Console.WriteLine("2. Registrarse");
-Console.Write("Tu opción: ");
-int option = Convert.ToInt32(Console.ReadLine());
+    {
+    Console.WriteLine("=== LogIn / SignUp ===");
+    Console.WriteLine("1. Iniciar Sesion");
+    Console.WriteLine("2. Registrarse");
+    Console.Write("Tu opción: ");
+    int option = Convert.ToInt32(Console.ReadLine());
 
-if (option == 1)
-{
-Console.Write("\nUsuario: ");
-string? username = Console.ReadLine();
-Console.Write("Contraseña: ");
-string? password = Console.ReadLine();
+    if (option == 1)
+    {
+    Console.Write("\nUsuario: ");
+    string? username = Console.ReadLine();
+    Console.Write("Contraseña: ");
+    string? password = Console.ReadLine();
 
-string tipoUsuario = ObtenerTipoUsuario(username, password);
+    string tipoUsuario = ObtenerTipoUsuario(username, password);
 
-if (!string.IsNullOrEmpty(tipoUsuario))
-{
-loggedIn = true;
-Console.WriteLine(" ¡Bienvenido, " + username + "!");
+    if (!string.IsNullOrEmpty(tipoUsuario))
+    {
+    loggedIn = true;
+    Console.WriteLine(" ¡Bienvenido, " + username + "!");
 
-if (tipoUsuario == "Estudiante")
-{
-Estudiante alumno = new Estudiante();
-alumno.MostrarOpciones();
-}
-else if (tipoUsuario == "Docente")
-{
-Docente docente = new Docente();
-docente.MostrarOpciones();
-}
-else if (tipoUsuario == "Almacenista")
-{
-Almacenista almacenista = new Almacenista();
-almacenista.MostrarOpciones();
-}
-else if (tipoUsuario == "Coordinador")
-{
-Coordinador coordinador = new Coordinador();
-coordinador.MostrarOpciones();
-}
-}
-else
-{
-Console.WriteLine("Usuario o Contraseña Incorrectos \n");
-}
+    if (tipoUsuario == "Estudiante")
+    {
+    Estudiante alumno = new Estudiante();
+    alumno.MostrarOpciones();
+    }
+    else if (tipoUsuario == "Docente")
+    {
+    Docente docente = new Docente();
+    docente.MostrarOpciones();
+    }
+    else if (tipoUsuario == "Almacenista")
+    {
+    Almacenista almacenista = new Almacenista();
+    almacenista.MostrarOpciones();
+    }
+    else if (tipoUsuario == "Coordinador")
+    {
+    Coordinador coordinador = new Coordinador();
+    coordinador.MostrarOpciones();
+    }
+    }
+    else
+    {
+    Console.WriteLine("Usuario o Contraseña Incorrectos \n");
+    }
 
-}
-else if (option == 2)
-{
-// Registro
-}
+    }
+    else if (option == 2)
+    {
+    // Registro
+    }
 }
 
 
 bool Login(string username, string password)
 {
 
-using (Almacen db = new())
-{
+    using (Almacen db = new())
+    {
 
-var Usuarios = db.Usuarios
-.Where(e => e.Usuario1.Equals(username)).Where(e => e.Password.Equals(password));
+        var Usuarios = db.Usuarios
+        .Where(e => e.Usuario1.Equals(username)).Where(e => e.Password.Equals(password));
 
-if ((Usuarios is null) || !Usuarios.Any())
-{
-return false;
-}
-else
-{
-return true;
-}
-}
-}
-
-string ObtenerTipoUsuario(string username, string password)
-{
-using (Almacen db = new Almacen())
-{
-var query = from usuario in db.Usuarios
-where usuario.Usuario1 == username && usuario.Password == password
-select new
-{
-TipoEstudiante = db.Estudiantes.Any(e => e.UsuarioId == usuario.UsuarioId),
-TipoDocente = db.Docentes.Any(e => e.UsuarioId == usuario.UsuarioId),
-TipoAlmacenista = db.Almacenista.Any(e => e.UsuarioId == usuario.UsuarioId),
-TipoCoordinador = db.Coordinadors.Any(e => e.UsuarioId == usuario.UsuarioId)
-};
-
-var result = query.FirstOrDefault();
-
-if (result != null)
-{
-if (result.TipoEstudiante) return "Estudiante";
-if (result.TipoDocente) return "Docente";
-if (result.TipoAlmacenista) return "Almacenista";
-if (result.TipoCoordinador) return "Coordinador";
+        if ((Usuarios is null) || !Usuarios.Any())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
 
-return string.Empty;
-}
+    string ObtenerTipoUsuario(string username, string password)
+    {
+    using (Almacen db = new Almacen())
+    {
+    var query = from usuario in db.Usuarios
+    where usuario.Usuario1 == username && usuario.Password == password
+    select new
+    {
+    TipoEstudiante = db.Estudiantes.Any(e => e.UsuarioId == usuario.UsuarioId),
+    TipoDocente = db.Docentes.Any(e => e.UsuarioId == usuario.UsuarioId),
+    TipoAlmacenista = db.Almacenista.Any(e => e.UsuarioId == usuario.UsuarioId),
+    TipoCoordinador = db.Coordinadors.Any(e => e.UsuarioId == usuario.UsuarioId)
+    };
+
+    var result = query.FirstOrDefault();
+
+    if (result != null)
+    {
+    if (result.TipoEstudiante) return "Estudiante";
+    if (result.TipoDocente) return "Docente";
+    if (result.TipoAlmacenista) return "Almacenista";
+    if (result.TipoCoordinador) return "Coordinador";
+    }
+
+    return string.Empty;
+    }
 }
 
 
